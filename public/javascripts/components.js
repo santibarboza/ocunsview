81f7e10ed15be51e77bb21547d4b09e0b0c4b0dd
Santi <santii.barboza@gmail.com>
2018-05-12 13:43:52 -0300

    inicio vue

--- public/javascripts/components.js
+++ public/javascripts/components.js
@@ -3,6 +3,41 @@ var contenedorCode = new Vue({
   data: {
     ver: true,
     bigSize:'col-md-4'
+  },
+  methods: {
+    ocultar:
+          function ocultarCode(){
+/*
+            if(this.ver){
+                this.ver=false;
+                if(contenedorCode.ver && contenedorSimulacion.ver)
+                    contenedorCode.bigSize='col-md-7';
+                else if(!contenedorSimulacion.ver)
+                        contenedorCode.bigSize='col-md-12';
+                    else
+                        contenedorSimulacion.bigSize='col-md-12';
+           }
+*/  
+     },
+    mostrar:
+          function mostrarCode(){
+/*  
+          if(!this.ver){
+                this.ver=true;
+                this.bigSize='col-md-3';
+                if(contenedorCode.ver && contenedorSimulacion.ver)
+                    contenedorCode.bigSize='col-md-4';
+                else if(contenedorCode.ver)
+                            contenedorCode.bigSize='col-md-9';
+                     else if(contenedorSimulacion.ver){
+                            this.bigSize='col-md-4';
+                            contenedorSimulacion.bigSize='col-md-9';
+                        }else {
+                            this.bigSize='col-md-12';
+                        }
+           }
+*/ 
+      }
   }
 })
 var contenedorCompilado = new Vue({
@@ -12,7 +47,7 @@ var contenedorCompilado = new Vue({
     bigSize:'col-md-3'
   },
   methods: {
-    ocultarCompilado:
+    ocultar:
           function ocultarCompilado(){
             if(this.ver){
                 this.ver=false;
@@ -24,18 +59,19 @@ var contenedorCompilado = new Vue({
                         contenedorSimulacion.bigSize='col-md-12';
            }
        },
-    mostrarCompilado:
+    mostrar:
           function mostrarCompilado(){
             if(!this.ver){
                 this.ver=true;
                 this.bigSize='col-md-3';
                 if(contenedorCode.ver && contenedorSimulacion.ver)
                     contenedorCode.bigSize='col-md-4';
-                else if(contenedorSimulacion.ver)
-                        contenedorSimulacion.bigSize='col-md-9';
-                    else if(contenedorCode.ver)
+                else if(contenedorCode.ver)
                             contenedorCode.bigSize='col-md-9';
-                        else{
+                     else if(contenedorSimulacion.ver){
+                            this.bigSize='col-md-4';
+                            contenedorSimulacion.bigSize='col-md-8';
+                        }else {
                             this.bigSize='col-md-12';
                         }
            }
@@ -47,6 +83,39 @@ var contenedorSimulacion = new Vue({
   data: {
     ver: true,
     bigSize:'col-md-5'
+  },
+  methods: {
+    ocultar:
+          function ocultarSimulacion(){
+            if(this.ver){
+                this.ver=false;
+                if(contenedorCode.ver && contenedorCompilado.ver){
+                    contenedorCode.bigSize='col-md-8';
+                    contenedorCompilado.bigSize='col-md-4';
+                }else if(!contenedorCompilado.ver)
+                        contenedorCode.bigSize='col-md-12';
+                    else
+                        contenedorCompilado.bigSize='col-md-12';
+           }
+     },
+    mostrar:
+          function mostrarSimulacion(){
+          if(!this.ver){
+                this.ver=true;
+                this.bigSize='col-md-5';
+                if(contenedorCode.ver && contenedorCompilado.ver){
+                    contenedorCode.bigSize='col-md-4';
+                    contenedorCompilado.bigSize='col-md-3';
+                }else if(contenedorCode.ver)
+                        contenedorCode.bigSize='col-md-7';
+                     else if(contenedorCompilado.ver){
+                            this.bigSize='col-md-8';
+                            contenedorSimulacion.bigSize='col-md-4';
+                        }else {
+                            this.bigSize='col-md-12';
+                        }
+           }
+      }
   }
 })
 var panelRegistros = new Vue({
