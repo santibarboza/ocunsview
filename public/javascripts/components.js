2fa59dbbe24bd0ee3b5288f8215000044fee697a
Santi <santii.barboza@gmail.com>
2018-05-12 10:50:31 -0300

    inicio vue

---
+++ public/javascripts/components.js
@@ -0,0 +1,33 @@
+var contenedorCode = new Vue({
+  el: '#contenedorCode',
+  data: {
+    ver: true,
+    bigSize:'col-md-4'
+  }
+})
+var contenedorCompilado = new Vue({
+  el: '#contenedorCode',
+  data: {
+    ver: true,
+    bigSize:'col-md-3'
+  }
+})
+var contenedorSimulacion = new Vue({
+  el: '#contenedorSimulacion',
+  data: {
+    ver: true,
+    bigSize:'col-md-5'
+  }
+})
+var panelRegistros = new Vue({
+  el: '#panelRegistros',
+  data: {
+    ver: true
+  }
+})
+var panelMemoria = new Vue({
+  el: '#panelMemoria',
+  data: {
+    ver: true
+  }
+})
