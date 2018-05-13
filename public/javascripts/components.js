66004c20464ac5411099942781b2832294318dfc
Santi <santii.barboza@gmail.com>
2018-05-13 16:57:37 -0300

    mostrar ocultar

--- public/javascripts/components.js
+++ public/javascripts/components.js
@@ -7,36 +7,39 @@ var contenedorCode = new Vue({
   methods: {
     ocultar:
           function ocultarCode(){
-/*
+
             if(this.ver){
                 this.ver=false;
-                if(contenedorCode.ver && contenedorSimulacion.ver)
-                    contenedorCode.bigSize='col-md-7';
-                else if(!contenedorSimulacion.ver)
-                        contenedorCode.bigSize='col-md-12';
+                if(contenedorCompilado.ver && contenedorSimulacion.ver){
+                    contenedorCompilado.bigSize='col-md-4';
+                    contenedorSimulacion.bigSize='col-md-8';
+                 }else if(!contenedorSimulacion.ver)
+                        contenedorCompilado.bigSize='col-md-12';
                     else
                         contenedorSimulacion.bigSize='col-md-12';
            }
-*/  
+  
      },
     mostrar:
           function mostrarCode(){
-/*  
+  
           if(!this.ver){
                 this.ver=true;
-                this.bigSize='col-md-3';
-                if(contenedorCode.ver && contenedorSimulacion.ver)
-                    contenedorCode.bigSize='col-md-4';
-                else if(contenedorCode.ver)
-                            contenedorCode.bigSize='col-md-9';
-                     else if(contenedorSimulacion.ver){
-                            this.bigSize='col-md-4';
-                            contenedorSimulacion.bigSize='col-md-9';
+                this.bigSize='col-md-4';
+                if(contenedorCompilado.ver && contenedorSimulacion.ver){
+                    contenedorCompilado.bigSize='col-md-3';
+                    contenedorSimulacion.bigSize='col-md-5';
+                }else if(contenedorCompilado.ver){
+                            this.bigSize='col-md-9';
+                            contenedorCompilado.bigSize='col-md-3';
+                     }else if(contenedorSimulacion.ver){
+                            this.bigSize='col-md-9';
+                            contenedorSimulacion.bigSize='col-md-5';
                         }else {
                             this.bigSize='col-md-12';
                         }
            }
-*/ 
+ 
       }
   }
 })
